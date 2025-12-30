6724d8873023c471bff3dfd2756a486668eb6f30
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2025-12-30 14:44:31 +0100

    Revert "feat: separate test from integration test"
    This reverts commit 3bc8b92b09de106480b8ac26c33973175b389ca0.

--- src/test/java/com/github/altfatterz/todoapp/TodoAppTests.java
+++ src/test/java/com/github/altfatterz/todoapp/TodoAppTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class TodoAppIT {
+class TodoAppTests {
 
     @Test
     void contextLoads() {
